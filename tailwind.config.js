d54f71425a1c92628c682aa88f375bc411cc67f2
C0RT1CERA <93507300+C0RT1CERA@users.noreply.github.com>
2022-06-26 05:28:09 +0530

    Added 4 pallet for D & W.
    2 pallets, each contain 4 color each belongs to Paint_Dark 1-0,1-1
    2 pallets, each contain 4 color each belongs to Paint_Light 1-2,1-3
    Dark=000
    Light=500

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'Paint_Dark': {
+        100: '#980F5A',
+        200: '#750550',
+        300: '#570530',
+        400: '#4C0027',
+        101: '#041C32',
+        201: '#04293A',
+        301: '#04293A',
+        401: '#041C32',
+        000: '#EADEDE',
+        },
+        'Paint_Light': {
+          102: '#FEE3EC',
+          202: '#F47C7C',
+          302: '#F999B7',
+          402: '#F2789F',
+          103: '#FFF323',
+          203: '#FFCA03',
+          303: '#FF5403',
+          403: '#F90716',
+          500: '#000000',
+          },
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
