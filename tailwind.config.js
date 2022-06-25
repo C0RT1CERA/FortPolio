d9f628f8dd61b246c741686213f062c0acd2f2bb
C0RT1CERA <93507300+C0RT1CERA@users.noreply.github.com>
2022-06-26 03:33:06 +0530

    Externals
    Externals

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'tahiti': {
+        100: '#cffafe',
+        200: '#a5f3fc',
+        300: '#67e8f9',
+        400: '#22d3ee',
+        500: '#06b6d4',
+        600: '#0891b2',
+        700: '#0e7490',
+        800: '#155e75',
+        900: '#164e63',
+        },
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
